99cb928c33841c02455ec1292082908895af7083
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-06-17 12:12:50 -0300

    create note controller

---
+++ src/controllers/note.js
@@ -0,0 +1,57 @@
+const { Note } = require("../models");
+
+module.exports = {
+  create(req, res) {
+    const { title, content, userId } = req.body;
+
+    try {
+      await Note.create({ title, content, userId });
+      return res.status(201);
+    } catch (error) {
+      console.log(error);
+      return res.status(500);
+    }
+  },
+
+  read(req, res) {
+    try {
+      const notes = user.findAll();
+
+      return req.status(200).json({ notes });
+    } catch (error) {
+      console.log(error);
+      return res.status(500);
+    }
+  },
+
+  update(req, res) {
+    const id = req.params;
+    const { title, content } = req.body;
+
+    try {
+      const note = await None.findOne(id);
+
+      if (!note) return req.status(404);
+
+      note.title = title;
+      note.content = content;
+
+      await note.save();
+
+      return res.status(204);
+    } catch (error) {
+      return res.status(500);
+    }
+  },
+
+  delete(req, res) {
+    const id = req.params;
+
+    try {
+      await Note.delete(id);
+      return res.status(204);
+    } catch (error) {
+      return res.status(500);
+    }
+  },
+};
