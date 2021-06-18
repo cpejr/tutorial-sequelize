925f7934bfc7f86a24bff2561026d5ff27de145a
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-06-17 23:08:40 -0300

    name relation with notes

--- src/models/user.js
+++ src/models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.Note);
+      User.hasMany(models.Note, { as: "notes" });
     }
   }
   User.init(
